b60ad0df3f3a9bd2612ff0fe091e10422291c8e8
Leonardo Andrade <leo.sbrf@gmail.com>
2018-09-17 04:35:34 +0000

    finalizacao do exercicio random-math

--- src/App.js
+++ src/App.js
@@ -2,15 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 class App extends Component {
-  render() {
+    
+  componentDidMount() {
+    if (!this.state.gameLoaded) {
+      this.loadGame();
+    }
+  }
+  nextNumber = () => {
+    return Math.floor(Math.random() * 100);
+  }
+
+  nextProposedAnswer = (value1, value2, value3) => {
+    return Math.floor(Math.random() * 3) + value1 + value2 + value3;
+  }
+
+  state = {
+    value1: 0,
+    value2: 0,
+    value3: 0,
+    proposedAnswer: 0,
+    numQuestions: -1,
+    numCorrect: 0,
+    gameLoaded: false
+  }
+
+  setNextRound = (incrementNumCorrect) => {
+    let nextValue1 = this.nextNumber();
+    let nextValue2 = this.nextNumber();
+    let nextValue3 = this.nextNumber();
+    let nextProposedAnswer = this.nextProposedAnswer(nextValue1, nextValue2, nextValue3);
+    
+    this.setState((currentState) => (
+      {
+        value1: nextValue1,
+        value2: nextValue2,
+        value3: nextValue3,
+        proposedAnswer: nextProposedAnswer,
+        numQuestions: currentState.numQuestions + 1,
+        numCorrect: currentState.numCorrect + incrementNumCorrect,
+        gameLoaded: true
+      }));
+  }
+
+  answer = (userAnswer) => {
+    let sum = this.state.value1 + this.state.value2 + this.state.value3;
+    let isProposedAnswerCorrect = sum === this.state.proposedAnswer;
+	let incrementCorrectAnswer = 0;
+    
+    //XNOR operation
+    if (isProposedAnswerCorrect === userAnswer)
+      incrementCorrectAnswer = 1;
+    
+    this.setNextRound(incrementCorrectAnswer);
+  }
+
+  loadGame = () => {
+    this.setNextRound(0);
+  }
+
+  render() {   
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -20,12 +73,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={() => this.answer(true)}>True</button>
+          <button onClick={() => this.answer(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
